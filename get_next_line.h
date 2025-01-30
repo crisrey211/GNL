6d46418afe4dcf520a66e2263a76ffb6558757e1
Christian Reynaltt <90900049+crisrey211@users.noreply.github.com>
2025-01-30 12:25:40 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -19,5 +19,11 @@
 # include <stdio.h>
 
 char	*get_next_line(int fd);
-
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_free(char **str);
 #endif
